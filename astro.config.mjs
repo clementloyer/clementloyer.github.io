0f63d7f06b07dd65e3bbeee9c38468872bd5c747
Clement Loyer <loyerclement@icloud.com>
2026-08-04 16:41:26 +0200

    fix: build2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://clementloyer.github.io',
+  // base: '/<repository-name>', // Only include this line if this is NOT a user/organization site (e.g., repo site)
+});
